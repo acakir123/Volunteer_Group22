950d317460c29ad330bce43077c448cbc55c230f
Marshall <marshallhmorgan@gmail.com>
2025-02-05 14:07:34 -0600

    admin dashboard view

--- Volunteer_Group22/AdminViews/AdminDashboardView.swift
+++ Volunteer_Group22/AdminViews/AdminDashboardView.swift
@@ -1,7 +1,82 @@
 import SwiftUI
 
 struct AdminDashboardView: View {
+    
+    @EnvironmentObject var authViewModel: AuthViewModel
+    
     var body: some View {
-        Text("Dashboard.")
+        ZStack {
+            Color.blue.opacity(0.1)
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            VStack {
+                HStack {
+                    Text("Welcome, Admin")
+                        .font(.system(size: 32))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.circle")
+                        .font(.system(size: 32))
+                        .padding(.trailing, 10)
+                }
+                .padding(.leading, 25)
+                .padding(.top, 20)
+                .padding(.bottom, 35)
+                
+                
+                HStack {
+                    Text("Overview")
+                        .font(.system(size: 24))
+                    
+                }
+                
+                Spacer()
+                
+                
+                VStack {
+                    Text("Quick Actions")
+                        .font(.system(size: 24))
+                    
+                    HStack {
+                        VStack {
+                            Image(systemName: "pencil.circle")
+                                .font(.system(size: 18))
+                            
+                            Text("Create Event")
+                                .font(.system(size: 18))
+                        }
+                        Spacer()
+                        
+                        Image(systemName: "pencil.circle")
+                            .font(.system(size: 18))
+                        
+                        Spacer()
+                        
+                        Image(systemName: "pencil.circle")
+                            .font(.system(size: 18))
+                    }
+                    .padding(.vertical, 5)
+                    .padding(.horizontal, 25)
+                }
+                .background(Color.black.opacity(0.2))
+                
+                
+                Spacer()
+                
+                HStack {
+                    Text("Recent Activities")
+                        .font(.system(size: 24))
+                }
+                
+                Spacer()
+            }
+        }
     }
+}
+
+#Preview {
+    AdminDashboardView()
+        .environmentObject(AuthViewModel())
 }
--- Volunteer_Group22/MainViews/ContentView.swift
+++ Volunteer_Group22/MainViews/ContentView.swift
@@ -6,7 +6,9 @@ struct ContentView: View {
     var body: some View {
         Group {
             if authViewModel.userSession != nil {
-                VolunteerMainTabView()
+                
+                
+                SignInView()
                 
                 
                 /* if user.role == admin {
@@ -16,7 +18,7 @@ struct ContentView: View {
                  }
                  */ 
             } else {
-                SignInView()
+                VolunteerMainTabView()
             }
         }
     }
