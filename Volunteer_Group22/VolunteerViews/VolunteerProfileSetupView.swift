1fe5096b185f8fe789dfe413cf3dd184e7f4d1ed
Thien Nguyen <thienphat0493@gmail.com>
2025-02-05 19:42:50 -0600

    linking VolunteerViews

--- Volunteer_Group22/VolunteerViews/VolunteerProfileSetupView.swift
+++ Volunteer_Group22/VolunteerViews/VolunteerProfileSetupView.swift
@@ -2,9 +2,16 @@ import SwiftUI
 
 struct VolunteerProfileSetupView: View {
     var body: some View {
-        Text("VolunteerProfileSetupView.")
+        Text("Volunteer Profile Setup View.")
+        
+        NavigationLink {
+            VolunteerEventSearchView()
+                .navigationBarBackButtonHidden(true)
+        } label: {
+            Image(systemName: "arrowshape.turn.up.backward.badge.clock")
+            Text("Back")
+        }
     }
-    
     
 }
 
